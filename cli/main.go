719c5b203f6993743825e014b1515ea69625fa33
p3ngf3i <pengfei@57blocks.com>
2024-09-09 14:00:07 +0800

    Revert "feat: add cli wallet cmd (#26)"
    This reverts commit 329cddbd1af23edd5ba8ca0f6cfb9a89f663c1a4.

--- cli/main.go
+++ cli/main.go
@@ -5,7 +5,6 @@ import (
 	_ "github.com/57blocks/auto-action/cli/internal/command/general"
 	_ "github.com/57blocks/auto-action/cli/internal/command/lambda"
 	_ "github.com/57blocks/auto-action/cli/internal/command/oauth"
-	_ "github.com/57blocks/auto-action/cli/internal/command/wallet"
 )
 
 func main() {
