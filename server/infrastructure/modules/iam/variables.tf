972a70267a371b3d41f1a4cd184711e49619aed7
v3nooonn <v3nooom@outlook.com>
2024-09-14 12:42:33 +0800

    blocked

---
+++ server/infrastructure/modules/iam/variables.tf
@@ -0,0 +1,15 @@
+variable "role_name" {
+  default = ""
+}
+
+variable "assume_role_policy" {
+  default = ""
+}
+
+variable "role_policy_name" {
+  default = ""
+}
+
+variable "policy" {
+  default = ""
+}
