517c90f30ce72b854532a391021366f04a72a089
p3ngf3i <v3nooom@outlook.com>
2024-09-23 18:58:33 +0800

    Chore(terraform): shell upgrade (#84)

--- infrastructure/update.sh
+++ infrastructure/update.sh
@@ -1,34 +1,27 @@
 #!/bin/bash
 
-# Set your variables
+## Set your variables
 REGION="<REGION>"
 CLUSTER="<CLUSTER>"
 SERVICE="<SERVICE>"
 TASK_DEFINITION="<TASK_DEFINITION>"
 ECS_TASK_EXECUTION_ROLE_ARN="<ECS_TASK_EXECUTION_ROLE_ARN>"
 
-
 # Describe the task definition and save to a JSON file
+TASK_DEF_ARN=$(
 aws ecs describe-task-definition \
   --region "$REGION" \
   --task-definition "$TASK_DEFINITION" \
-  --query 'taskDefinition.{containerDefinitions:containerDefinitions}' \
-  --output json > task-definition.json
-
-TASK_DEF_ARN=$(
-aws ecs register-task-definition \
-  --region "$REGION" \
-  --family "$TASK_DEFINITION" \
-  --network-mode awsvpc \
-  --cpu 1024 \
-  --memory 2048 \
-  --requires-compatibilities FARGATE \
-  --execution-role-arn "$ECS_TASK_EXECUTION_ROLE_ARN" \
-  --cli-input-json file://task-definition.json \
   --query 'taskDefinition.taskDefinitionArn' \
   --output text
 )
 
+if [[ $? -ne 0 || -z "$TASK_DEF_ARN" ]]; then
+  echo "Failed to describe the task definition" >&2
+  exit 1
+fi
+
+# Update the service by the same task definition
 aws ecs update-service \
   --region "$REGION" \
   --cluster "$CLUSTER" \
@@ -36,5 +29,7 @@ aws ecs update-service \
   --task-definition "$TASK_DEF_ARN" \
   --force-new-deployment
 
-# Clean up the JSON file if needed
-rm task-definitions.json
+if [[ $? -ne 0 ]]; then
+  echo "Failed to update the service" >&2
+  exit 1
+fi
